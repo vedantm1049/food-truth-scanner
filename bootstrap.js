c25db57c157997c161356ec10bd05756da594a54
vedantm1049 <vedant.m1049@gmail.com>
2026-09-01 01:40:55 +0400

    Compose curated trust feature

--- bootstrap.js
+++ bootstrap.js
@@ -11,6 +11,7 @@ function bootstrapFoodTruthScanner() {
   installOpenFoodFactsUI();
   installPersonalization();
   installNutrientUI();
+  installCuratedTrust();
   installScanner();
   installBranding();
   render();
